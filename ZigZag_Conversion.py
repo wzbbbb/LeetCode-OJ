b9d21c7189763aff7a4bcf075616d125aa6a7133
Zhi Bing Wang <zhibwang@cisco.com>
2018-09-19 15:54:31 -0700

    new file:   ZigZag_Conversion.py

---
+++ ZigZag_Conversion.py
@@ -0,0 +1,32 @@
+s = '123456789abcdef'
+nRows = 3
+'''A better way to do this, form a list of string and attach new char to them
+For example:
+n = 5 =>  1,2,3,4,5,4,3,2
+n = 4 => 1,2,3,4,3,2
+'''
+
+def convert(s, nRows):
+  n=nRows
+  leng=len(s)
+  pattern_length = n + n - 2
+  A = [''] * n
+  pattern = []
+  for i in range(n):
+    pattern.append(i)
+  for i in range(n-2, 0, -1):
+    pattern.append(i)
+  print pattern
+  i = 0
+  while i < leng:
+    for j in range(pattern_length):
+      print 'j',j, 'i',i
+      A[pattern[j]] += s[i]
+      i += 1
+      if i == leng:  break 
+    print A
+  ss=''
+  for a in A:
+    ss += a
+  return ss
+print convert(s, nRows)
