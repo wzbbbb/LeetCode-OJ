c976fa556038f5ae92cd2c5a62246f8652e3bff5
Zhi Bing Wang <zhibwang@cisco.com>
2018-09-26 11:24:48 -0700

    modified:   Maximum_Subarray.py

--- Maximum_Subarray.py
+++ Maximum_Subarray.py
@@ -1,10 +1,11 @@
-A = [-2,1,1,-3,4,-1,2,1,-5,4]
+#A = [-2,1,1,-3,4,-1,2,1,-5,4]
+A = [-2,-11,-1,-3,-4,-1,-2,-1,-5,-4]
 
 def max_subarray(A):
   max1= None
   sum1 = None
   for a in A:
-    if sum1 == None or sum1 < 0 and a > 0:
+    if sum1 == None or (sum1 < 0 and sum1 < a):
       sum1 = a
     else: 
       sum1 += a
