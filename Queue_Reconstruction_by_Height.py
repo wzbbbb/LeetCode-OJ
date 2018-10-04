f088776331e88ca8a528404839303ac451519de7
Zhi Bing Wang <zhibwang@cisco.com>
2018-10-03 22:37:30 -0700

    new file:   Queue Reconstruction by Height.txt 	new file:   Queue_Reconstruction_by_Height.py

---
+++ Queue_Reconstruction_by_Height.py
@@ -0,0 +1,52 @@
+#people = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+people =  [[8,2],[4,2],[4,5],[2,0],[7,2],[1,4],[9,1],[3,1],[9,0],[1,0]]
+def reconstructQueue(people):
+  ''' sort by (x[1],x[0]), then the first item is good for sure
+      then check and move item one by one based on x[1]'''
+  # This one exceed time limit :(
+  A = sorted(people, key=lambda x:(x[1],x[0]))
+  n = len(A)
+  i = 1
+  while i < n:
+    moving = True
+    while moving:
+      count = 0
+      for j in range(i):
+        if A[j][0] >= A[i][0]:
+          count +=1
+      if count > A[i][1] :
+        A[i], A[i-1] = A[i-1], A[i]
+        i -= 1
+      elif count == A[i][1] :
+        moving = False
+        i +=1
+  return A
+#print reconstructQueue(people)
+
+''' Leetcode solution
+Pick out tallest group of people and sort them in a subarray (S). Since there's no other groups of people taller than them, therefore each guy's index will be just as same as his k value.
+For 2nd tallest group (and the rest), insert each one of them into (S) by k value. So on and so forth.
+E.g.
+input: [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+subarray after step 1: [[7,0], [7,1]]
+subarray after step 2: [[7,0], [6,1], [7,1]]
+'''
+# Submission Result: Accepted
+def requeue(people):
+  A = sorted(people, key = lambda x:(x[0],x[1]))
+  n = len(A)
+  B = []*n
+  C = []     #all, currently, tallest ones taken from A
+  while A:
+    C.append(A.pop())
+    while A: 
+      if A[-1][0] == C[0][0]:
+        C.append(A.pop())
+      else: 
+        break
+    C.sort(key = lambda x:x[1], reverse=True)
+    while C:
+      i = C[-1][1]
+      B.insert(i,C.pop() )
+  return B
+print requeue(people)
