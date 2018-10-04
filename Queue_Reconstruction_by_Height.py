015c0720d17add735560f9f3ea1690077d8e69fe
Zhi Bing Wang <zhibwang@cisco.com>
2018-10-03 22:44:13 -0700

    modified:   Queue_Reconstruction_by_Height.py

--- Queue_Reconstruction_by_Height.py
+++ Queue_Reconstruction_by_Height.py
@@ -1,5 +1,5 @@
-#people = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
-people =  [[8,2],[4,2],[4,5],[2,0],[7,2],[1,4],[9,1],[3,1],[9,0],[1,0]]
+people = [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+#people =  [[8,2],[4,2],[4,5],[2,0],[7,2],[1,4],[9,1],[3,1],[9,0],[1,0]]
 def reconstructQueue(people):
   ''' sort by (x[1],x[0]), then the first item is good for sure
       then check and move item one by one based on x[1]'''
@@ -33,17 +33,23 @@ subarray after step 2: [[7,0], [6,1], [7,1]]
 '''
 # Submission Result: Accepted
 def requeue(people):
+  ''' So the idea is that sort by hight 
+      Then find all people with the same hight
+      then insert them based on its existing rank value
+      lower ranked people insert first, for example, [5,0] before [5,2]
+  ''' 
   A = sorted(people, key = lambda x:(x[0],x[1]))
   n = len(A)
   B = []*n
   C = []     #all, currently, tallest ones taken from A
   while A:
     C.append(A.pop())
-    while A: 
+    while A:                    # filling C with the same hight people
       if A[-1][0] == C[0][0]:
         C.append(A.pop())
       else: 
         break
+    print '==',C
     C.sort(key = lambda x:x[1], reverse=True)
     while C:
       i = C[-1][1]
