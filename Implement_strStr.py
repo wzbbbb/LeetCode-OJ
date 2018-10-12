f44e1e9ed19a212f4f011140d3014cf5dff2fe71
Zhi Bing Wang <zhibwang@cisco.com>
2018-10-12 12:20:59 -0700

    modified:   Implement_strStr.py 	new file:   Implement_strStr.txt

--- Implement_strStr.py
+++ Implement_strStr.py
@@ -9,3 +9,18 @@ def find(haystack, needle):
       return i
 print find(haystack, needle)
 
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        n = len(haystack)
+        m = len(needle)
+        if m == 0:
+          return 0
+        for i in range(n - m + 1):
+          if haystack[i:i+m] == needle:
+            return i
+        return -1
